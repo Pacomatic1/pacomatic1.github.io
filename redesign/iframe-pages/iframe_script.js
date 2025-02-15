829ba600820fe5f368230a0cfac5d7e0aa33dbd6
Pacomatic1 <ammarzawar123@gmail.com>
2025-02-14 23:54:33 -0500

    fonts. 7.css added, unused... for now!

--- redesign/iframe-pages/iframe_script.js
+++ redesign/iframe-pages/iframe_script.js
@@ -1,9 +1,16 @@
-addCssToChildPage();
+addMandatoryBitsToChildPage();
 
-function addCssToChildPage() {
+
+
+function addMandatoryBitsToChildPage() {
+	// Add CSS stylesheet via link tag.
+	// I could have each page manually link it but any and all common code should ideally be offloaded to this script, the parent page, and the stylesheet that this script is appending.
 	var cssLink = document.createElement("link");
 	cssLink.href = "iframe_styling.css"; 
 	cssLink.rel = "stylesheet"; 
 	cssLink.type = "text/css";
-	document.head.appendChild(cssLink);	
+	document.head.appendChild(cssLink);
+
+
+
 }
