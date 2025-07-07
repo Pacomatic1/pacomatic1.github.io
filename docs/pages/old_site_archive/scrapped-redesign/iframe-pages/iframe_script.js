d8c087d2d5e28c639bee01bc9905a2616e8dc20f
Pacomatic1 <ammarzawar123@gmail.com>
2025-07-06 23:04:28 -0400

    Started on Projects Page
    I'm basing it off the scrapped redesign. As such it, uhh...
    sucks.

---
+++ docs/pages/old_site_archive/scrapped-redesign/iframe-pages/iframe_script.js
@@ -0,0 +1,20 @@
+// This is kept in the same spot as the iframe-pages in order to simplify the <script> link, since that's done manually (and I want to reduce the amount of things done manually by as much as possible).
+addMandatoryBitsToChildPage();
+
+
+
+function addMandatoryBitsToChildPage() {
+	// Add CSS stylesheet via link tag.
+	// I could have each page manually link it but any and all common code should ideally be offloaded to this script, the parent page, and the stylesheet that this script is appending.
+	var cssLink = document.createElement("link");
+	cssLink.href = "./resources/iframe_styling.css"; 
+	cssLink.rel = "stylesheet"; 
+	cssLink.type = "text/css";
+	document.head.appendChild(cssLink);
+
+	// iframe-resizer's child script is added in order to avoid a scrollbar for the childPage.
+	var iframeResizerChildLink = document.createElement("script");
+	iframeResizerChildLink.src = "./resources/iframe-resizer-child.js"; 
+	document.head.appendChild(iframeResizerChildLink);
+
+}
