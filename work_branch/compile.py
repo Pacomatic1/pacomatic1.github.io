38a03221da672a91360c00e25c557222882206ce
Pacomatic1 <ammarzawar123@gmail.com>
2025-08-05 02:40:11 -0400

    Rebuilding SitesILike using new system
    This will make it faster, and it will work as a simple test for this 'limited static site generation' approach.
    I can't wait to go online for help, only to horrifyingly realize that this is an already-done, well-established, battle-tested concept that people have used for decades.

--- work_branch/compile.py
+++ work_branch/compile.py
@@ -1,3 +1,10 @@
-print("Time to compile! You'd better have Node.js, npm, and Python (though you likely already do TwT) installed! Otherwise, you're doomed!")
-print("On an uneralted note: Why do I do this to myself?")
+from subprocess import call
+import os
 
+# Use 'os.system(commandYouWannaRun)' to run terminal commands. You know, do the thing this is built to do.
+
+print("Time to compile! You'd better have Node.js, npm, and Python (though you likely already do TwT) installed! Otherwise, you're doomed!")
+print("Also make sure you're located in the same directory in this file! Otherwise, you will die.")
+print("On an unrelated note: Why do I do this to myself?")
+
+os.system('python "./pages/sites_i_like/compile.py"')
