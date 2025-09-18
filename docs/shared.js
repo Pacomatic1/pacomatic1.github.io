e20231a9c481ec6050f690e262d6324a18ff94cc
Pacomatic1 <74610266+Pacomatic1@users.noreply.github.com>
2025-09-18 10:14:36 -0400

    UI: Fixed Landing Page Icon
    Tested main mode on Firefox. Turns out having an image source as 'whitepsace' causes it to break (where as chrome doesn't care).

--- docs/shared.js
+++ docs/shared.js
@@ -5,7 +5,7 @@ const navBarItemLookupTable = [
 
     // If [0] is 0, it's a button.
         // [1] is the buttons's name. Note that there is no margin between the icon and the text, so you're gonna have to do it yourself using custom styling. Accessible mode has no margins, so don't add a space before the item's name.  
-        // [2] is exclusive to main mode and is the **relative path** to the button's icon, starting from this page. Keep in mind that this icon is done with the 'src' attribute of an <img> tag inside the button tag, so make sure that your icon is suppported by the <img>. If you don't want an icon, leave it blank. 
+        // [2] is exclusive to main mode and is the **relative path** to the button's icon, starting from this page. Keep in mind that this icon is done with the 'src' attribute of an <img> tag inside the button tag, so make sure that your icon is suppported by the <img>. If you don't want an icon, do not put anything inside. Yes, that includes whitespace; some browsers dislike that.
         // [3] and [4] control what the item does.
             // If [3] is 0, then it will be a button and [4] will be a link that gets opened inside the iframe.
             // If [3] is 1, then it will be a button and [4] will be a link that replaces the current tab and will get rid of this whole single-page thing. I repeat, it will open in the current tab and get rid of this site.
@@ -17,7 +17,7 @@ const navBarItemLookupTable = [
         // [2] is the text's styling. "1" makes it a navBar section. If it's left as a blank string while [1] contains text, it will be irrelevant. If [1] is blank, this will be irrelevant. Exclusive to main mode.
         // [3] is whether or not we should append a horizontal rule, using true or false. If you just want a horizontal rule, leave everything else blank.
         // Note that in accessible mode, just text or text and a horizontal rule will create a new option group. Breaking out of that option group is done by just creating a horizontal rule. I do not recommend nesting them.
-    [0, "Landing Page", " ", 0, "./pages/landing/index.html", ""],
+    [0, "Landing Page", "", 0, "./pages/landing/index.html", ""],
     [0, "Home", "assets/navbar-icons/3ds-home-menu-icon.svg", 0, "./pages/home/index.html", "margin-bottom:-2px; height: auto; width: 16px; margin-right: 4px;"],
     [0, "Sites I Like", "", 0, "./pages/sites_i_like/index.html", ""],
     [0, "Projects", "", 0, "./pages/projects/index.html", ""]
