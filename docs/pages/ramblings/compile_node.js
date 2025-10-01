baf2cfe44e8123eba8a4b3999bc654bf7e625daf
Pacomatic1 <74610266+Pacomatic1@users.noreply.github.com>
2025-10-01 11:54:34 -0400

    Began file finding on Ramblings compile script

--- docs/pages/ramblings/compile_node.js
+++ docs/pages/ramblings/compile_node.js
@@ -1,8 +1,57 @@
-const Asciidoctor = require('asciidoctor')
-const asciidoctor = Asciidoctor()
+// Find all files needed, while making sure to read them with the correct encoding.
+// Run all the posts through asciidoctor.
 
-const content = 'http://asciidoctor.org[*Asciidoctor*] ' +
-  'running on https://opalrb.com[_Opal_] ' +
-  'brings AsciiDoc to Node.js!'
-const html = asciidoctor.convert(content) // (2)
-console.log(html) 
+// Use post_base.html, and have them get injected into there. Keep in mind that post_base.html also has some important stuff around dates and titles, so don't forget those.
+// Put the results of each post in index.html, and put them in the same folder that their AsciiDoc file is located.
+// Then, once every post is complete, put everything into the main base.html.
+
+
+
+
+
+const Asciidoctor = require('asciidoctor');
+const asciidoctor = Asciidoctor();     // Using this is done through assciidoctor.convert(content: string).
+const fs = require("node:fs");
+
+
+console.log("Ramblings: Started");
+
+// Find files
+var homePagePath = "./base.html";
+var postFolderPath = "./posts/"
+var postPaths = [];
+
+// postFolders will, if unmodified, return files too. We do not want this. As such, we gotta get rid of 'em.
+var postFolders = fs.readdirSync(postFolderPath, {withFileTypes: true});
+var postFoldersTempArray = [];
+for (const index in postFolders) {    
+    if ( postFolders[index].isDirectory() ) {
+        postFoldersTempArray.push(postFolders[index]);
+    }
+}
+postFolders = postFoldersTempArray;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Ramblings: Done");
